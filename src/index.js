85393ffcb1128d0306039260f145e133b7927f3f
zeku87 <nocomment182@gmail.com>
2018-01-29 12:38:28 +0100

    Components Structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'reacat-dom';
+//Nuevo componente
+const App = () => {
+	return <div>Hola</div>;
+}
+
+
+//Renderizando....
+ReactDOM.render(<App/>, document.getElementsByTagName("body"));
