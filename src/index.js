bb70ddda1a1a6c5b1d36f50aad3c15925e7c470b
zeku87 <nocomment182@gmail.com>
2018-02-01 15:06:43 +0100

    Youtube search response

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import youtube from 'youtube-api-search'
 import SearchBar from './components/SearchBar';
 
-const API_KEY = '9e8fyA9s8dduDge5ioSDDug89RGeg3iou453tXg';
+const API_KEY = '<API KEY>';
 
 
 //Nuevo componente
-const App = () => {
-	return (
+class App extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {videos: []};
+
+		youtube({key: API_KEY, term: "Avril Lavigne"}, videos => {
+			this.setState({ videos }); // videos:videos
+			console.log(this.state.videos);
+		});
+
+	}
+
+
+	render(){
+		return (
 		<div>
 			<SearchBar />
 		</div>
 		);
+	}
 }
 
 
